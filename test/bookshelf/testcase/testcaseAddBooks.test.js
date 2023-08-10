3501a6bb05a682722e4368430151797917576fe1
Muhammad Akbar <akbarmhmmmd@gmail.com>
2023-08-11 01:58:07 +0700

    fix: fix testcaseAddBooks

--- test/bookshelf/testcase/testcaseAddBooks.test.js
+++ test/bookshelf/testcase/testcaseAddBooks.test.js
@@ -7,14 +7,9 @@ const numRetries = 1;
 const tokenLoginAccess = `Bearer ${data.token.tokenLogin}`;
 
 describe('User Add Books Positive Scenario', () => {
-  let bookResponse;
-
-  beforeAll(async () => {
-    bookResponse = await endpointAddBooks.postBooks(tokenLoginAccess);
+  retry(data.testCase.BookshelfAddBook.positive.addBookWithCompleteData, numRetries, async () => {
+    const bookResponse = await endpointAddBooks.postBooks(tokenLoginAccess);
     global.bookId = bookResponse._body.data.bookId;
-  });
-
-  retry(data.testCase.BookshelfAddBook.positive.addBookWithCompleteData, numRetries, () => {
     //console.log('bookResponse:', bookResponse);
     expect(bookResponse._body.status).to.equal(data.message.successMessage);
     expect(bookResponse._body.message).to.equal(data.message.successAddBook);
